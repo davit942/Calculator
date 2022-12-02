025ec38d5d8ffef3383ee0a4b2a83a65e269b65f
Davit <zkac151@live.rhul.ac.uk>
2022-12-02 22:38:05 +0000

    beginning to implement RevPolish

--- src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
+++ src/main/java/uk/ac/rhul/cs2800/RevPolishCalc.java
@@ -1,5 +1,20 @@
 package uk.ac.rhul.cs2800;
 
-public class RevPolishCalc {
+public class RevPolishCalc implements Calculator {
+
+  private NumStack values;
+
+  public RevPolishCalc() {
+    this.values = null;
+  }
+
+  @Override
+  public float evaluate(String what) throws InvalidExpressionException {
+    if (what == null || what.equals("")) {
+      throw new InvalidExpressionException("string is empty or null");
+    }
+    return 0;
+  }
+
 
 }
