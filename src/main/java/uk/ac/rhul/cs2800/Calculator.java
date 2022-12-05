9d98d6749d2780ea8ab08bcbced15831362fcc04
Davit <zkac151@live.rhul.ac.uk>
2022-12-05 04:53:55 +0000

    merging calc interface and exception from other branch

---
+++ src/main/java/uk/ac/rhul/cs2800/Calculator.java
@@ -0,0 +1,16 @@
+package uk.ac.rhul.cs2800;
+
+/**A simple interface to allow each calculator to evaluate an expression.
+ * @author zkac151
+ * 
+ */
+public interface Calculator {
+
+  /**This method will evaluate the expression.
+   * @param what the expression
+   * @return the answer
+   * @throws InvalidExpressionException when a wrong expression form is inputed
+   */
+  float evaluate(String what) throws InvalidExpressionException;
+
+}
